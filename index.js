63e40bef3cabbd6ef46f6bb021ac8d0d48426f8e
natyyp <natypiresoliveira@gmail.com>
2023-04-26 17:16:52 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,77 @@
+// let primeiro = 5
+// let segundo = 4
+
+
+// let resultado = (primeiro - segundo) * -1
+
+// console.log(resultado)
+
+//  let primeiro = 234
+// let segundo = 5
+
+// let resultado = 234 % 5
+
+// console.log(resultado)
+
+
+// const numero1 = Number(prompt("digite um numero"))
+// const numero2 = Number(prompt("digite outro numero"))
+// console.log("iguais", numero1 === numero2 );
+// console.log('diferente' , numero1 !== numero2);
+// console.log("maior" , numero1 > numero2);
+// console.log("menor" , numero1 < numero2);
+//  const nome = prompt("qual seu nome?")
+//  const anosNascimento = Number(prompt("ano que nasceu?"))
+//  const idade = new Date().getFullYear() -anosNascimento
+//  const maioDiIdade = idade >= 18
+//  const diferensaAnus = 2050 - new Date().getFullYear()
+//  console.log("nome :", nome)
+//  console.log("idade :", idade)
+//  console.log("é maior de idade :", maioDiIdade)
+//  console.log("quantos anos terá em 2050 :", diferensaAnus + idade)
+
+
+// let primeiroNumero = Number (prompt("Digite um numero!"))
+// let segundoNumero = Number (prompt("Digite outro numero!"))            
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+
+//  const suaidade = Number (prompt("qual sua idade!"))
+//  const idadeamigo = Number (prompt("qual idade do seu amigo?"))
+//  let maioindade = suaidade >= idadeamigo
+//  let diferensaedadi = suaidade - idadeamigo
+
+//  console.log("sua idade: " , suaidade)
+//  console.log("idade do seu amigo(a): " , idadeamigo)
+//  console.log("sua idade é maior do que a do seu amigo?" , suaidade > idadeamigo)
+
+ 
+
+//  let numeropa = Number(prompt("escreva um numero par!"))
+
+//  console.log(numeropa % 2 === 0);
+
+
+// const idade = Number(prompt("digite sua idade"))
+
+// const idadiinmezes = idade * 12
+// const idadeindiaz = idade * 365 * 24
+
+// console.log("idade:" , idade)
+// console.log("idade em meses:" , idadiinmezes)
+// console.log("idade em dias:" , idadeindiaz)
+
+
+// const numero1 = Number(prompt("digite um numero!"))
+// const numero2 = Number(prompt("digite outro numero!"))
+
+// console.log(" maior que 2" , numero1 > 2)
+// console.log(" igual a 2" , numero1 === numero2)
+// console.log(" divisivel por 2" , numero1 % numero2 === 0)
+// console.log(" divisivel por 1" , numero2 % numero1 === 0)
+
+
+
